38b72c83cbbe151efe5d6e285daa1381da26030d
Akshay Pratap Singh <128460095+akshay124-pixel@users.noreply.github.com>
2025-06-28 10:02:39 +0530

    Update AddEntry.js

--- src/components/AddEntry.js
+++ src/components/AddEntry.js
@@ -449,7 +449,7 @@ function AddEntry({ onSubmit, onClose }) {
     try {
       setLoading(true);
       const response = await axios.post(
-        "https://sales-order-furniture-server.onrender.com/api/orders",
+        "https://sales-order-furniture-server-4ucn.onrender.com/api/orders",
         newEntry,
         {
           headers: {
