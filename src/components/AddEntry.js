12003b8adf227a160bb746e3d4231ad931f116a1
Akshay Pratap Singh <akshay124-pixel@example.com>
2025-08-22 12:34:16 +0530

    Done

--- src/components/AddEntry.js
+++ src/components/AddEntry.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useMemo } from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { Spinner } from "react-bootstrap";
